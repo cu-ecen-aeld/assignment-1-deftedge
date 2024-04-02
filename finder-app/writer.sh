357fc23885afc0da073eb3754710c83964cd20c1
George Johnson <deftedge.mail@gmail.com>
2024-04-01 19:11:47 -0700

    Added directory check

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -8,8 +8,17 @@ if [ "$#" -ne 2 ]; then
     echo "    2)String to be written to the file"
     exit 1
 fi
+
 FULL_FILE_PATH=$1  # e.g. /tmp/aesd/assignment1/sample.txt
 TEXT_STRING=$2  # text to write to the file
+
+# Extract directory path
+directory=$(dirname "$FULL_FILE_PATH")
+
+# Check if the directory exists, create it if not
+if [ ! -d "$directory" ]; then
+    mkdir -p "$directory"
+fi
 
 echo "$TEXT_STRING" > $FULL_FILE_PATH
 
@@ -17,6 +26,5 @@ if [ "$?" -eq 0 ]; then
     echo "success"
     exit 0
 else
-    echo "failed: expected ${} in ${} but instead"
     exit 1
 fi
